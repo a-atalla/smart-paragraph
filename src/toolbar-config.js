85514e7b8f2c9ec47f5f3eba63cf218e50e07289
a.atalla <a.3talla@gmail.com>
2019-09-27 21:41:39 +0200

    add more toolbuttons

--- src/toolbar-config.js
+++ src/toolbar-config.js
@@ -1,13 +1,16 @@
-import { BLOCK_TYPE, INLINE_STYLE } from 'draftail';
+import { BLOCK_TYPE, INLINE_STYLE, ENTITY_TYPE } from 'draftail';
 
 export const inlineStyles = [
   { type: INLINE_STYLE.BOLD, icon: <span class="dashicons dashicons-editor-bold"></span> },
   { type: INLINE_STYLE.ITALIC, icon: <span class="dashicons dashicons-editor-italic"></span> },
-  { type: INLINE_STYLE.CODE, icon: <span class="dashicons dashicons-editor-code"></span> },
-  { type: INLINE_STYLE.QUOTATION, icon: <span class="dashicons dashicons-format-quote"></span> },
+  { type: INLINE_STYLE.UNDERLINE, icon: <span class="dashicons dashicons-editor-underline"></span> },
+  { type: INLINE_STYLE.STRIKETHROUGH, icon: <span class="dashicons dashicons-editor-strikethrough"></span> },
+  { type: INLINE_STYLE.CODE, icon: <span class="dashicons dashicons-editor-code"></span> },  
 ];
 
 export const blockTypes = [
   { type: BLOCK_TYPE.UNORDERED_LIST_ITEM, icon: <span class="dashicons dashicons-editor-ul"></span> },
   { type: BLOCK_TYPE.ORDERED_LIST_ITEM, icon: <span class="dashicons dashicons-editor-ol"></span> },
+  { type: BLOCK_TYPE.BLOCKQUOTE, icon: <span class="dashicons dashicons-format-quote"></span>},
 ];
+
